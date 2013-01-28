3e6dd0ab2b23e2b0c72571d5bc3989cf5697cd35
Pontus Granström <pnutus@gmail.com>
2013-01-28 21:53:36 +0100

    Updated imports file to reflect recommendations.

--- imports.py
+++ imports.py
@@ -1,3 +1,7 @@
+# As recommended at
+# http://docs.scipy.org/doc/scipy/reference/tutorial/general.html
+
 import scipy as sp
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
