db546e1ae40b1ee6d9080e7ba27040966af873c2
Pontus Granström <pnutus@gmail.com>
2013-01-31 10:51:46 +0100

    Added our own (not moshinsky's) calculation of H elements. Not currently working.

--- HO_basis.py
+++ HO_basis.py
@@ -8,7 +8,6 @@ def Kdelta(x, y):
 @sp.vectorize
 def H_element(n, n_prim, l, s, eps, V):
     """Returns matrix element of the Hamiltonian"""
-    #n' => m; n => n
     # http://docs.sympy.org/0.7.2/modules/physics/sho.html
     rsq = eps**2 * .5 * ( \
         sp.sqrt(n * (n + l + .5)) * Kdelta(n_prim, n - 1) + \
@@ -22,26 +21,50 @@ def H_element(n, n_prim, l, s, eps, V):
     (integral, _) = integrate.fixed_quad(integrand, 0, 10, n = 20)
     return rsq + sp.sqrt(2) * eps * integral
 
+@sp.vectorize
+def H_element2(n, n_prim, l, s, omega, V):
+    """Returns matrix element of the Hamiltonian"""
+    m = 1
+    result = 0
+    if n == n_prim:
+        result += omega * (2*n + l + 1.5)
+    elif n_prim == (n - 1):
+        result += m * omega**2 / 4 * sp.sqrt( n*(n + l + .5) )
+    elif n_prim == (n + 1):
+        result += m * omega**2 / 4 * sp.sqrt( (n + 1)*(n + l + 1.5) )
+    
+    nu = m * omega * .5
+    
+    @sp.vectorize
+    def integrand(r):
+        return R_nl(n_prim, l, nu, r) * V(r, l, s) * R_nl(n, l, nu, r) * r**2.
+    
+    (integral, _) = integrate.fixed_quad(integrand, 0, 10, n = 20)
+    return result + float(integral)
+
 
 def optimized_eps(V):
     """Returns the epsilon that gives the minimum ground energy"""
     @sp.vectorize
     def ground_energy(eps):
         """First matrix element as a function of epsilon"""
-        return H_element(0, 0, 0, .5, eps, V)
-    res = optimize.minimize_scalar(ground_energy, bounds=(0, 10), method='bounded')
+        return H_element2(0, 0, 0, .5, eps, V)
+    res = optimize.minimize_scalar(ground_energy, bounds=(0, 2), method='bounded')
     return float(res.x) # 1.06384608107
 
 def hamiltonian(V, N, verbose=False):
     """Calculates the N x N hamiltonian matrix."""
     eps = optimized_eps(V)
+    if verbose:
+        print "omega", eps
     H = sp.zeros((N, N))
     for i in range(N):
         if verbose:
             print "rad", i+1
         for j in range(i+1):
-            H[i, j] = H[j, i] = H_element(i, j, 0, .5, eps, V)
+            H[i, j] = H[j, i] = H_element2(i, j, 0, .5, eps, V)
     return H
+
 
 def energies(H):
     return linalg.eigh(H, eigvals_only = True)
--- H_moshinsky.py
+++ H_moshinsky.py
@@ -4,6 +4,18 @@ import HO_basis
 @sp.vectorize
 def V(r, l, s):
     return - 1. / r
+    
+# @sp.vectorize
+# def ground_energy(eps):
+#     """First matrix element as a function of epsilon"""
+#     return HO_basis.H_element2(0, 0, 0, .5, eps, V)
+# 
+# x =  sp.linspace(0, 4)
+# plt.plot(x, ground_energy(x))
+# plt.show()
 
 H = HO_basis.hamiltonian(V, 10, verbose=True)
-print HO_basis.energies(H)
+E = HO_basis.energies(H)
+print "H =", H
+print "E =", E
+print "GT = ", E[0] * 2.
