d8432c2c61d96cfad1ec3f5a915d8eb9f60d6ea0
Pontus Granström <pnutus@gmail.com>
2013-01-31 16:33:04 +0100

    Removed old comment.

--- H_moshinsky.py
+++ H_moshinsky.py
@@ -4,15 +4,6 @@ import HO_basis
 @sp.vectorize
 def V(r, l, s):
     return - 1. / r
-    
-# @sp.vectorize
-# def ground_energy(eps):
-#     """First matrix element as a function of epsilon"""
-#     return HO_basis.H_element2(0, 0, 0, .5, eps, V)
-# 
-# x =  sp.linspace(0, 4)
-# plt.plot(x, ground_energy(x))
-# plt.show()
 
 H = HO_basis.hamiltonian(V, 10, verbose=True)
 E = HO_basis.energies(H)
