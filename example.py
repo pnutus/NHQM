28c4123322f645263caa378a62ff79beee29e5f7
Pontus Granström <pnutus@gmail.com>
2013-02-08 16:54:44 +0100

    Fleshed out example.

--- example.py
+++ example.py
@@ -1,17 +1,22 @@
 from imports import *
-import basis_mom_space
-import basis_harm_osc
-import problem_He5
+import basis_mom_space as mom
+import basis_harm_osc as osc
+import problem_He5 as He5
+import problem_H_atom as H_atom
 import calculate_serial as calc
 
-problem = problem_He5.problem
 
+problems = H_atom.problem, He5.problem
+order = 20
 l = 0
 j = .5
 
-H_mom = calc.hamiltonian(basis_mom_space.H_element, args=(problem, l, j))
-print "MomSpace lowest energy:", calc.energies(H_mom)[0] * problem.eV_factor, "eV"
+for problem in problems:
+    H_mom = calc.hamiltonian(mom.H_element, args=(problem, l, j), order=order)
+    lowest_energy = calc.energies(H_mom)[0] * problem.eV_factor
+    print "MomSpace lowest energy:", lowest_energy, "eV"
 
-omega = basis_harm_osc.optimal_osc_freq(problem, l, j)
-H_osc = calc.hamiltonian(basis_harm_osc.H_element, args=(problem, omega, l, j))
-print "HarmOsc lowest energy:", calc.energies(H_osc)[0] * problem.eV_factor, "eV"
+    omega = osc.optimal_osc_freq(problem, l, j)
+    H_osc = calc.hamiltonian(osc.H_element, args=(problem, omega, l, j), order=order)
+    lowest_energy = calc.energies(H_osc, hermitian=True)[0] * problem.eV_factor
+    print "HarmOsc lowest energy:", lowest_energy, "eV"
