4fa1b120458776fef981a8d3145342e757e7c866
Pontus Granström <pnutus@gmail.com>
2013-02-26 15:08:10 +0100

    Cleaned up scripts.

--- scripts/berggren_real.py
+++ scripts/berggren_real.py
@@ -1,7 +1,6 @@
 from __future__ import division
 from imports import *
 from nhqm.bases import mom_space as mom
-from nhqm.bases import berggren as berg
 from nhqm.problems import He5
 from nhqm.calculations import QM as calc
 
@@ -15,7 +14,4 @@ contour = sp.linspace(0, 2.5, order + 1)
 args = (problem, l, j)
 H = calc.contour_hamiltonian(mom.H_element_contour, contour, args)
 eigvals, eigvecs = calc.energies(H)
-print "MomSpace:", eigvals[0]
-H = calc.contour_hamiltonian(berg.H_element, contour, args)
-eigvals, eigvecs = calc.energies(H)
-print "Berggren:", eigvals[0]
+print "Berggren on the real line, lowest energy:", eigvals[0]
