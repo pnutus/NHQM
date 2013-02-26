e66e7c677328fe5c583522047de81c5231974b75
Pontus Granström <pnutus@gmail.com>
2013-02-26 13:38:23 +0100

    Berggren real test script.

--- scripts/berggren.py
+++ scripts/berggren.py
@@ -1,26 +1,21 @@
 from __future__ import division
 from imports import *
 from nhqm.bases import mom_space as mom
-from nhqm.bases import berggren_contour as berg
+from nhqm.bases import berggren as berg
 from nhqm.problems import He5
 from nhqm.calculations import QM as calc
-
-def gen_contour(tip_x, tip_y, k_max, points):
-    """
-    Generates a iosceles triangle contour 
-    with tip at (tip_x, tip_y) which then 
-    keeps going from (2*tip_x, 0) to (k_max, 0),
-    """
-    return berg.gen_berggren_contour(tip_x, - abs(tip_y), 
-                k_max, points, first_zero_position = 2*tip_y)
 
 problem = He5.problem   
 order = 30
 l = 1
 j = 1.5
-problem.V0 = -47.
-step_size 
+problem.V0 = -70.
 
-contour = gen_contour(0.17, 0.04, 2.5, order)
-args = (problem, )
-H = calc.hamiltonian(berg.H_complex_element, )
+contour = sp.linspace(0, 2.5, order + 1)
+args = (problem, l, j)
+H = calc.contour_hamiltonian(mom.H_element_contour, contour, args)
+eigvals, eigvecs = calc.energies(H)
+print "MomSpace:", eigvals[0]
+H = calc.contour_hamiltonian(berg.H_element, contour, args)
+eigvals, eigvecs = calc.energies(H)
+print "Berggren:", eigvals[0]
