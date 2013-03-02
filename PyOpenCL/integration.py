d727cd8becf3434b727c5384ee4fa97c279e0b89
Vincent Ericsson <ericssonvincent@gmail.com>
2013-03-02 02:17:51 +0100

    Included comments of the code.

--- PyOpenCL/integration.py
+++ PyOpenCL/integration.py
@@ -6,13 +6,16 @@ import scipy as sp
 from pyopencl.reduction import ReductionKernel
 from pyopencl.elementwise import ElementwiseKernel
 
-# Must-have-variables for OpenCL
-ctx = cl.create_some_context()
+# Make shure we use the GPU for computations.
+platform=cl.get_platforms()
+gpu_devices=platform[0].get_devices(device_type=cl.device_type.GPU)
+ctx=cl.Context(devices=gpu_devices)
 queue = cl.CommandQueue(ctx)
 
+# Integration, one integral, using reduce
 length=0.001
 vals=cl_array.arange(queue,0,100,length,dtype=numpy.float32)
-
+# Kernel for reduce-code
 krnlRed=ReductionKernel(ctx,numpy.float32,neutral="0",
     reduce_expr="a+b",map_expr="get_val(x[i])*%10.3f" % length,
     arguments="__global float *x",
@@ -23,10 +26,13 @@ krnlRed=ReductionKernel(ctx,numpy.float32,neutral="0",
     }
     """)
 
-tonum=1000000
+# Generation of an array where each element is an evaluated integral.
+tonum=1000000 # Number of elements.
+# Array to send to the GPU.
 p_gpu=cl_array.to_device(ctx,queue,sp.linspace(0,tonum,tonum+1).astype(numpy.float32))
-res=cl_array.empty_like(p_gpu)
-    
+res=cl_array.empty_like(p_gpu) # The resultating array
+
+# Elementwise (mapping) kernel.
 krnlMap=ElementwiseKernel(ctx,"float *param, float *res", "res[i]=integrate(param[i])",preamble="""
     float integrate(float param)
     {
@@ -38,9 +44,10 @@ krnlMap=ElementwiseKernel(ctx,"float *param, float *res", "res[i]=integrate(para
         return sum/1000.0;
     }
 """)
-integrand=krnlRed(vals).get()
-krnlMap(p_gpu,res)
+integrand=krnlRed(vals).get() # Calculate the first integral.
+krnlMap(p_gpu,res) # Generate the large array.
 
+# Print results
 print "Value of integral: ", integrand
 print "x**2 from 0 to 100\n"
 print "''Integrand''"
