eec548ce39bae11d2ce875fb4cbafe11bf365fae
Pontus Granström <pnutus@gmail.com>
2013-03-04 07:36:20 +0100

    Fixed bugs in mom_space, removed contour stuff.

--- nhqm/bases/mom_space.py
+++ nhqm/bases/mom_space.py
@@ -8,24 +8,22 @@ name = "MomSpace"
 
 def j_l(l, k):
     """Spherical bessel."""
-    if l == 0:
-        l1 = 1
-    else:
-        l1 = l
+    l1 = 1 if l == 0 else l
     _, j_l, _, _, _ = csphjy(l1, k) 
     return j_l[l]
 
 @sp.vectorize
 def integrand(r, k, k_prim, V, l, j):
-    return r**2 * j_l(l, sp.conj(k)*r) \
+    # k = sp.conj(k) WHAT
+    return r**2 * j_l(l, k*r) \
             * j_l(l, k_prim*r) * V(r, l, j)
     
-def H_element_contour(k, k_prim, step, problem, l = 0, j = .5):
+def H_element_contour(k, k_prim, weight, problem, l = 0, j = .5):
     diagonal = k**2 / (2 * problem.mass) * (k == k_prim)
     V = problem.potential
     integral, _ = fixed_quad(integrand, 0, 10, \
                                 n = 20, args=(k, k_prim, V, l, j))
-    return diagonal + 2 * k_prim**2 * step / sp.pi * integral
+    return diagonal + 2 * k_prim**2 * weight / sp.pi * integral
 
 def H_element(n, n_prim, step, *args, **kwargs):
     k, k_prim = [(x + 1)*step for x in (n, n_prim)]
@@ -33,23 +31,8 @@ def H_element(n, n_prim, step, *args, **kwargs):
 
 def gen_basis_function(problem, l = 0, j = .5):
     # TODO
-    # contour/complex
     # Not normalized!
-    def basis_function(r, k, step):
-        return 4*sp.pi / (2*sp.pi)**1.5 * k**2 * step * j_l(l, k*r)
+    # Doesn't work with G-L quadrature
+    def basis_function(r, k, weight):
+        return 4*sp.pi/(2*sp.pi)**1.5 * k**2 * weight*j_l(l, k*r)
     return basis_function
-
-
-# Berggren contour:    
-
-@sp.vectorize
-def triangle(x, peak_x, peak_y, k_max):
-    if x < 2*peak_x:
-        return peak_y * (abs(x/peak_x - 1) - 1)
-    else:
-        return 0
-
-def gen_simple_contour(peak_x, peak_y, k_max, points):
-    xs = sp.linspace(0, k_max, points + 1)[1:]
-    ys = triangle(xs, peak_x, peak_y, k_max)
-    return xs + 1j * ys
