0d157182cfa83a6efa57d185ae2f46349dc6ff14
Pontus Granström <pnutus@gmail.com>
2013-03-04 13:00:32 +0100

    Additions to contour script.

--- scripts/contour.py
+++ scripts/contour.py
@@ -14,6 +14,11 @@ peak_x = 0.17
 peak_y = 0.07
 k_max = 2.5
 
+def res_index(eigvecs):
+    maxes = map(max, eigvecs.T)
+    return maxes.index(min(maxes))
+        
+
 contour = calc.triangle_contour(peak_x, peak_y, k_max, order/3)
 #contour = calc.gauss_contour([0, k_max], order)
 #contour = calc.naive_triangle_contour(0, 0, k_max, order)
@@ -21,15 +26,20 @@ points, _ = contour
 args = (problem, l, j)
 H = calc.contour_hamiltonian(mom.H_element_contour, contour, args)
 eigvals, eigvecs = calc.energies(H)
-print "Berggren lowest energy:", eigvals[0]
+idx = res_index(eigvecs)
+print idx
+print "Berggren lowest energy:", eigvals[idx]
 basis_function = mom.gen_basis_function(args)
-wavefunction = calc.gen_wavefunction(eigvecs[:,0], basis_function, contour)
+wavefunction = calc.gen_wavefunction(eigvecs[:,idx], basis_function, contour)
 r = sp.linspace(0, 100, 1000)
 plt.subplot(311)
 plot_contour(contour)
 plot_poles(eigvals, problem.mass)
+pole = sp.sqrt(2*problem.mass*eigvals[idx])
+plt.plot(sp.real(pole), sp.imag(pole), 'ro')
 plt.subplot(312)
 plot_wavefunctions(contour, eigvecs)
+plt.plot(sp.real(points), calc.absq(eigvecs[:,idx]), 'r', linewidth=2)
 plt.subplot(313)
 plt.plot(r, r**2 * calc.absq(wavefunction(r)))
 plt.show()
