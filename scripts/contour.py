f91b2c06a9ae64a354beaa03c885665e31206068
Pontus Granström <pnutus@gmail.com>
2013-03-04 07:40:03 +0100

    Updated contour.py script.

--- scripts/contour.py
+++ scripts/contour.py
@@ -6,27 +6,30 @@ from nhqm.problems import He5
 from nhqm.calculations import QM as calc
 
 problem = He5.problem   
-order = 100
+order = 50
 l = 1
 j = 1.5
 problem.V0 = -47.
 peak_x = 0.17
-peak_y = 0.05
+peak_y = 0.07
 k_max = 2.5
 
-contour = mom.gen_simple_contour(peak_x, peak_y, k_max, order)
-
+contour = calc.triangle_contour(peak_x, peak_y, k_max, order/3)
+#contour = calc.gauss_contour([0, k_max], order)
+#contour = calc.naive_triangle_contour(0, 0, k_max, order)
+points, _ = contour
 args = (problem, l, j)
 H = calc.contour_hamiltonian(mom.H_element_contour, contour, args)
 eigvals, eigvecs = calc.energies(H)
 print "Berggren lowest energy:", eigvals[0]
 basis_function = mom.gen_basis_function(args)
 wavefunction = calc.gen_wavefunction(eigvecs[:,0], basis_function, contour)
-r = sp.linspace(0, 10)
-plt.figure(0)
-plt.plot(r, r**2 * calc.absq(wavefunction(r)))
-plt.figure(1)
+r = sp.linspace(0, 100, 1000)
+plt.subplot(311)
+plot_contour(contour)
 plot_poles(eigvals, problem.mass)
-plt.figure(2)
+plt.subplot(312)
 plot_wavefunctions(contour, eigvecs)
+plt.subplot(313)
+plt.plot(r, r**2 * calc.absq(wavefunction(r)))
 plt.show()
