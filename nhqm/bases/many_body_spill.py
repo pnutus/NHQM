2721c13d212505f06f1a020ebc770e8935eca00a
Nils Wireklint <nils.wireklint@gmail.com>
2013-04-10 19:21:13 +0200

    code clean up

--- nhqm/bases/many_body_spill.py
+++ nhqm/bases/many_body_spill.py
@@ -5,6 +5,7 @@ from itertools import combinations
 import mom_space as mom
 from collections import defaultdict
 import many_body as mb
+import fermion_state as fs
 
 
 def gen_states(s, num_particles):
@@ -43,37 +44,6 @@ def hamiltonian(sp_H, eigvecs, contour, num_particles=2):
              int(gd[0]), int(gd[1]) )
             H[key] += temp
     return H
-
-
-def get_sp_smart(num_states, num_particles, res_dict):
-    mb_states= gen_states(num_states, num_particles)
-    sp_states= gen_states(num_states, num_particles=1)
-    
-    #res_n =0    
-    
-    for bra_index, bra in enumerate(mb_states):
-
-        for alfa in bra:
-            
-            alpha=set([alfa])
-
-            for beta in (sp_states): 
-                """we'd like to generate beta from the set of possible sp_states minus the set of states already present in the (bra -alpha), can this be done through clever set manipulations?"""
-                if ((bra - alpha) - beta) == (bra - alpha):
-                    #can't add existing fermion 
-                    ket = (bra - alpha) | beta
-                    ket_index = mb_states.index(ket)
-                    res = (alpha, beta)
-                    
-                    #print res_n
-                    #res_n += 1
-                    #print res
-                    
-                    res_dict[bra_index, ket_index][0].append(res)
-    return  
-        
-"""def translate_ket_to_index( state_vector, ket):
-    return state_vector.index(ket)        """
         
 def get_2p_smart(num_states, num_particles, res_dict):
     mb_states = gen_states(num_states, num_particles)
@@ -111,17 +81,10 @@ def get_hamilton_dict(num_states, num_particles):
         
     h_dict = defaultdict( default_factory )
     get_2p_smart(num_states, num_particles, h_dict)
-    get_sp_smart(num_states, num_particles, h_dict)
     return h_dict
 
-def get_sp_dict(ns,np):
-    def default_factory():
-        return ([],[])
-    h_dict = defaultdict( default_factory )
-    get_sp_smart(ns,np,h_dict)
-    return h_dict  
-      
-def get_2p_dict(ns,np):
+
+    def get_2p_dict(ns,np):
     def default_factory():
         return ([],[])
     h_dict = defaultdict( default_factory )
@@ -129,29 +92,4 @@ def get_2p_dict(ns,np):
     return h_dict                   
     
 if __name__ == '__main__':
-        
-
-    ns =5
-    np =3
-    
-    res = get_hamilton_dict(ns,np) 
-    
-    mb_s = gen_states(ns,np)
-    a,b,c,d,e,f = 0,0,2,3,2,4
-    print mb_s[a], mb_s[b], "::", res[(a,b)][0]
-    print mb_s[c], mb_s[d], "::", res[(c,d)][0]
-    print mb_s[e], mb_s[f], "::", res[(e,f)][0]
-    print ""
-    print "2p:"
-    print ""
-    print mb_s[a], mb_s[b], "::", res[(a,b)][1]
-    print mb_s[c], mb_s[d], "::", res[(c,d)][1]
-    print mb_s[e], mb_s[f], "::", res[(e,f)][1]
-    
-    print ""
-    print""
-    print ""
-    print ":::::::::::::::::::::::::::"
-    
-    #for key in res:
-    #    print mb_s[key[0]], mb_s[key[1]], "::", res[key]
+    print "poor grammar makes me [sic]"
