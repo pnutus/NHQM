2eabed9df6142efeb94acd8820b9491e791db102
Pontus Granström <pnutus@gmail.com>
2013-04-13 11:58:59 +0200

    Generate many-body states with arbitrary quantum numbers.

--- nhqm/bases/fermion_state.py
+++ nhqm/bases/fermion_state.py
@@ -1,4 +1,5 @@
-from itertools import combinations
+from itertools import combinations, product
+from collections import namedtuple
 
 class FermionState:
     """
@@ -9,7 +10,7 @@ class FermionState:
     def __init__(self, states = []):
         self.states = []
         self.sign = 1
-        for state in states:
+        for state in reversed(states):
             self._create(state)
             
     def _create(self, new_state):
@@ -54,6 +55,9 @@ class FermionState:
     def __iter__(self):
         return iter(self.states)
         
+    def __contains__(self, value):
+        return value in self.states
+        
     def __str__(self):
         if self.sign == 0:
             return "0"
@@ -68,6 +72,9 @@ class FermionState:
         
     def __len__(self):
         return len(self.states)
+        
+    def __getitem__(self, key):
+        return self.states[key]
     
 def state_index(states, new_state):
     for i, state in enumerate(states):
@@ -77,6 +84,27 @@ def state_index(states, new_state):
             return i
     return len(states)
 
-# TESTS?
+def gen_mb_states(quantum_numbers, num_particles=2):
+    """
+    Generates many-body states given a list of 
+    quantum numbers and their possible values.
+    Like so [('m', [-1.5, -0.5, 0.5, 1.5]),
+             ('k', [0, 1, 2, 3, 4, 5, 6])]
+    """
+    names, values = zip(*quantum_numbers)
+    sp = namedtuple('sp', names)
+    sp_states = [sp(*tup) for tup in product(*values)]
+    return map(FermionState, combinations(sp_states, num_particles))
 
-print FermionState([(1,-.5), (1, .5), (7, 1.5), (0, -1.5)])
+if __name__ == '__main__':
+    # example
+    order = 10
+    q_nums = [('m', [-1.5, -0.5, 0.5, 1.5]),
+              ('k', range(order))]
+    mb_states = gen_mb_states(q_nums, 2)
+    print len(mb_states)
+    for mb_state in mb_states:
+        print mb_state
+
+
+# TESTS?
