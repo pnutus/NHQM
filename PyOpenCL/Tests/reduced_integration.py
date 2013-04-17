50a3f839bcea9a25e4f76e6da511530c6d17c9b5
Nils Wireklint <nils.wireklint@gmail.com>
2013-04-17 10:51:45 +0200

    added gauss legendre contour to reduce integration

--- PyOpenCL/Tests/reduced_integration.py
+++ PyOpenCL/Tests/reduced_integration.py
@@ -5,17 +5,28 @@ import numpy.linalg as la
 import scipy as sp
 from pyopencl.elementwise import ElementwiseKernel
 from pyopencl.reduction import ReductionKernel
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__),'../..'))
 
-size=10
+from nhqm.calculations import QM as calc
+
+order = 5000
+l = 1
+j = 1.5
+peak_x = 0.17
+peak_y = 0.07
+k_max = 2.5
+
+contour = calc.gauss_contour([0,3], order)
+points, weights = contour
+points = points.astype(numpy.float32)
+weights = weights.astype(numpy.float32)
 
 ctx=cl.create_some_context()
 queue=cl.CommandQueue(ctx)
 
-a = cl.array.arange(queue, 400, dtype=numpy.float32)
-b = cl.array.arange(queue, 400, dtype=numpy.float32)
-
-arr=(numpy.array([i for i in range(size)])).astype(numpy.float32)
-gpu_arr=cl_array.to_device(ctx,queue,arr)
+gpu_points=cl_array.to_device(ctx,queue,points)
+gpu_weights=cl_array.to_device(ctx,queue,weights)
 
 krnl = ReductionKernel(ctx, numpy.float32, neutral="0.0f",
         reduce_expr="a+b", map_expr="x[i]*func(y[i])",
@@ -23,9 +34,9 @@ krnl = ReductionKernel(ctx, numpy.float32, neutral="0.0f",
         preamble="""
         float func(float x)
             {
-                return 1.0f;
+                return pow(x,5);
             }
         """)
 
-my_dot_prod = krnl(gpu_arr, gpu_arr).get()
+my_dot_prod = krnl(gpu_weights, gpu_points).get()
 print my_dot_prod
