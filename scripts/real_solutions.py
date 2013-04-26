f1a945762148c44762454bcb914f317aa6e0f118
Ola Embreus <embreus@student.chalmers.se>
2013-04-26 17:24:04 +0200

    updated real_solutions

--- scripts/real_solutions.py
+++ scripts/real_solutions.py
@@ -23,7 +23,7 @@ problem.V0 = -52
 H = mom.hamiltonian(contour, problem, Q)
 eigvals_1, eigvecs_1 = energies(H)
 
-problem.V0=-47
+problem.V0=-50
 H = mom.hamiltonian(contour, problem, Q)
 eigvals_2, eigvecs_2 = energies(H)
 
@@ -34,7 +34,7 @@ r = sp.linspace(1e-1, rmax, r_order)
 
 def sqrd_wf(eigvec):
     wf = mom.gen_wavefunction(eigvec, contour, Q)
-    return r_order / rmax * r**2 * absq(wf(r))*30# / norm(r*wf)**2 
+    return r_order / rmax * r**2 * absq(wf(r))*10/ norm(r*wf(r))**2 
 
 def plotwf(eigvec, energy, color='k'):
     wf = sqrd_wf(eigvec)
@@ -43,46 +43,63 @@ def plotwf(eigvec, energy, color='k'):
 
 
 
-plt.figure(1)
-plt.clf()
-plt.title("Eigensolution wavefunctions. \n  $V0 = {0},\, N = {1}$".format(problem.V0, order), fontsize=20)
-plt.xlabel('r', fontsize=20)
-plt.ylabel('$r^2|R(r)|^2$', fontsize=20)
+#plt.figure(1)
+#plt.clf()
+#plt.title("Eigensolution wavefunctions. \n  $V0 = {0},\, N = {1}$".format(problem.V0, order), fontsize=20)
+#plt.xlabel('r', fontsize=20)
+#plt.ylabel('$r^2|R(r)|^2$', fontsize=20)
 
-problem.V0=-52
-V = Q.l * (Q.l + 1) / r**2 + problem.potential(r, Q.l, Q.j)
-plt.plot(r, V, '--k', linewidth=3)
+#plotwf(eigvecs_1[:,8], eigvals_1[8])
+#plotwf(eigvecs_1[:,17], eigvals_1[17])
+#plotwf(eigvecs_1[:,19], eigvals_1[19])
+#plotwf(eigvecs_1[:,20], eigvals_1[20])
 
-plotwf(eigvecs_1[:,10], eigvals_1[10])
-plotwf(eigvecs_1[:,14], eigvals_1[14])
-plotwf(eigvecs_1[:,18], eigvals_1[18])
-plotwf(eigvecs_1[:,7], eigvals_1[7])
+#plt.figure(2)
+#plt.clf()
+#plt.title("Eigensolution wavefunctions. \n  $V0 = {0},\, N = {1}$".format(problem.V0, order), fontsize=20)
+#plt.xlabel('r', fontsize=20)
+#plt.ylabel('Energy [MeV] / $r^2|R(r)|^2$', fontsize=20)
 
-plt.figure(2)
-plt.clf()
-plt.title("Eigensolution wavefunctions. \n  $V0 = {0},\, N = {1}$".format(problem.V0, order), fontsize=20)
-plt.xlabel('r', fontsize=20)
-plt.ylabel('Energy [MeV] / $r^2|R(r)|^2$', fontsize=20)
+#plotwf(eigvecs_2[:,13], eigvals_2[13])
+#plotwf(eigvecs_2[:,17], eigvals_2[17])
+#plotwf(eigvecs_2[:,19], eigvals_2[19])
+#plotwf(eigvecs_2[:,20], eigvals_2[20])
 
-problem.V0=-47
-V = Q.l * (Q.l + 1) / r**2 + problem.potential(r, Q.l, Q.j)
-plt.plot(r, V, '--k', linewidth=3)
+ 
+#plt.figure(3)
+#plt.clf()
+#plt.plot(abs(eigvecs_1))
+#plt.plot(abs(eigvecs_1[:,8]),'k', linewidth=3)
+ 
+#plt.figure(4)
+#plt.clf()
+#plt.plot(abs(eigvecs_2))
+#plt.plot(abs(eigvecs_2[:,13]),'k', linewidth=3)
 
-plotwf(eigvecs_2[:,10], eigvals_2[10])
-plotwf(eigvecs_2[:,14], eigvals_2[14])
-plotwf(eigvecs_2[:,18], eigvals_2[18])
-plotwf(eigvecs_2[:,7], eigvals_2[7])
 
-# 
-# plt.figure(3)
-# plt.clf()
-# plt.plot(abs(eigvecs_1))
-# plt.plot(abs(eigvecs_1[:,7]),'k', linewidth=3)
-# 
-# plt.figure(4)
-# plt.clf()
-# plt.plot(abs(eigvecs_2))
-# plt.plot(abs(eigvecs_2[:,7]),'k', linewidth=3)
+fig1=plt.figure(5)
+fig1.clf()
+
+ax1=fig1.add_subplot(121)
+wf_res = sqrd_wf(eigvecs_1[:,8])
+wf_1 = sqrd_wf(eigvecs_1[:,17])
+wf_2 = sqrd_wf(eigvecs_1[:,30])
+
+ax1.plot(r, wf_res, 'k', linewidth=3)
+ax1.plot(r, wf_1, 'k', linewidth=3) 
+ax1.axis([0, 100, 0, 1.6])
+
+ax1.get_yaxis().set_ticks([])
+
+ax2=fig1.add_subplot(122)
+wf_res = sqrd_wf(eigvecs_2[:,13])
+wf_1 = sqrd_wf(eigvecs_2[:,17])
+wf_2 = sqrd_wf(eigvecs_2[:,30])
+
+ax2.plot(r, wf_res, 'k', linewidth=3)
+ax2.plot(r, wf_1, 'k', linewidth=3) 
+ax2.axis([0, 100, 0, 1.6])
+ax2.get_yaxis().set_ticks([])
 
 plt.show()
 
