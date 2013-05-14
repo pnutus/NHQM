6e309760c9cf5f03f95df1c615dcb146ab6237da
Pontus Granström <pnutus@gmail.com>
2013-05-14 15:19:48 +0200

    Made harm_osc work with a contour

--- nhqm/bases/harm_osc.py
+++ nhqm/bases/harm_osc.py
@@ -10,6 +10,8 @@ integration_order = 70
 integration_range = 20
 
 def solution(order, problem, Q):
+    try: order = len(order[0])
+    except Exception: pass
     H = hamiltonian(order, problem, Q)
     eigvals, eigvecs = energies(H, hermitian=True)
     return eigvals, eigvecs
