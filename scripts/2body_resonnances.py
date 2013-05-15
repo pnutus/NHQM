ee32eef57df196951b38727d9b9a318b8d8250cc
Nils Wireklint <nils.wireklint@gmail.com>
2013-05-15 20:08:13 +0200

    updated 2b_res to work more as a utility script for 3b

--- scripts/2body_resonnances.py
+++ scripts/2body_resonnances.py
@@ -6,19 +6,16 @@ from nhqm.QM_helpers import absq, energies
 from nhqm.bases.gen_contour import gauss_contour, triangle_contour
 from collections import namedtuple
 
-
 k_max = 7
-
 
 problem = He5
 basis = mom
-basis_size = 15
+basis_size = 10
 bs = basis_size
-tail_size = 25
-ks =[[],[]]
-problem.V0 = -47
-#He5.Vso = -9.58
-
+tail_size = 20
+momenta =[[],[]]
+eigvals = [[],[]]
+eigvecs = [[],[]]
 corner_x = 0.5
 corner_y = -0.45j
 problem.V0 = -47.
@@ -31,30 +28,40 @@ def find_closest(vec, val):
     #returns index, value
     index= min(range(len(vec)), key=lambda i: abs(vec[i]-val))
     return index, vec[index]
-            
-contour = gauss_contour([0,corner_y, corner_x +corner_y, corner_x, k_max], [bs,bs,bs,tail_size] )
-QNums = namedtuple('qnums', 'l j J M E m')
-Qs = [QNums(l=1, j=1.5, J=0, M=0, 
-          m=[-1.5, -0.5, 0.5, 1.5], 
-          E=range(basis_size)),
-    QNums(l=1, j=0.5, J=0, M=0, 
-              m=[-1.5, -0.5, 0.5, 1.5], 
-              E=range(basis_size))]
-                    
-for m,Q in enumerate(Qs):
-    print Q.l, Q.j
-    
-    eigvals, eigvecs = basis.solution(contour, problem, Q)
-    for i in xrange(len(eigvals)):
-        ks[m].append(sp.sqrt(2*problem.mass*eigvals[i])    )
-        if sp.imag(ks[m][i]) > 0:
-            ks[m][i] = sp.conj(ks[m][i])
-        
-i32, res32 = find_closest(ks[0], p32res_exp)
-i12, res12 = find_closest(ks[1], p12res_exp)
-print "p3/2 ressonance:",res32
-print "p1/2 resonnance:",res12
 
-plt.plot(sp.real(ks[1]),sp.imag(ks[1]),'b*')
-plt.plot(sp.real(ks[1][i12]),sp.imag(ks[1][i12]),'r*')
-plt.show()
+def calc():
+
+            
+    contour = gauss_contour([0,corner_y, corner_x + corner_y, corner_x, k_max], [bs,bs,bs,tail_size] )
+    QNums = namedtuple('qnums', 'l j J M E m')
+    Qs = [QNums(l=1, j=0.5, J=0, M=0, 
+              m=[-1.5, -0.5, 0.5, 1.5], 
+              E=range(basis_size)),
+        QNums(l=1, j=1.5, J=0, M=0, 
+                  m=[-1.5, -0.5, 0.5, 1.5], 
+                  E=range(basis_size))]
+                    
+    for m,Q in enumerate(Qs):
+        eigvals[m], eigvecs[m] = basis.solution(contour, problem, Q)
+        for i in xrange(len(eigvals[m])):
+            momenta[m].append(sp.sqrt(2 * problem.mass * eigvals[m][i]) )
+            if sp.imag(momenta[m][i]) > 0:
+                momenta[m][i] = sp.conj(momenta[m][i])
+    return eigvals, eigvecs, momenta    
+        
+def export_resonance():
+    _, eigvecs, momenta = calc()
+    i32, res32 = find_closest(momenta[1], p32res_exp)
+    i12, res12 = find_closest(momenta[0], p12res_exp)
+    return eigvecs[0][i12],eigvecs[1][i32]    
+        
+if __name__ == '__main__':        
+    _, _, momenta = calc()
+    i32, res32 = find_closest(momenta[1], p32res_exp)
+    i12, res12 = find_closest(momenta[0], p12res_exp)
+    print "p3/2 ressonance:",res32
+    print "p1/2 resonnance:",res12
+
+    plt.plot(sp.real(momenta[1]),sp.imag(momenta[1]),'b*')
+    plt.plot(sp.real(momenta[1][i32]),sp.imag(momenta[1][i32]),'r*')
+    plt.show()
