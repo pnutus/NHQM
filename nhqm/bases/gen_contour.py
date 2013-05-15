aecaef0c5d655b2cf4926faa7126c7b28c70e684
Nils Wireklint <nils.wireklint@gmail.com>
2013-05-15 17:50:16 +0200

    improved gauss_cont to accept explicit segment orders

--- nhqm/bases/gen_contour.py
+++ nhqm/bases/gen_contour.py
@@ -2,15 +2,16 @@ from __future__ import division
 import scipy as sp
 from scipy.special.orthogonal import p_roots
 
-def gauss_contour(vertices, order):
+def gauss_contour(vertices, points_per_seg):
     """
     Generates a contour along the line segments between
     vertices using Gauss-Legendre quadrature.
     """
-    (x, w) = p_roots(order)
+    
     num_segments = len(vertices) - 1
     points = weights = sp.empty(0, complex)
     for i in range(num_segments):
+        (x, w) = p_roots(points_per_seg[i])
         a = vertices[i]
         b = vertices[i + 1]
         scaled_x = (x * (b - a) + (a + b))/2
@@ -20,8 +21,12 @@ def gauss_contour(vertices, order):
     return (points, weights)
     
 def triangle_contour(peak_x, peak_y, k_max, order):
+    return triangle_contour_explicit(peak_x, peak_y, k_max, 2*order, order)
+
+def triangle_contour_explicit(peak_x, peak_y, k_max, trig_order, tail_order):
     vertices = [0, peak_x - 1j*peak_y, 2*peak_x, k_max]
-    return gauss_contour(vertices, order)
+    points_per_seg = [trig_order/2, trig_order/2, tail_order]
+    return gauss_contour(vertices, points_per_seg)
     
 # OLD CODE AHEAD!
     
