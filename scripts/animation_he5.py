bdca0db84980a7c0ed2fe9597643a436711aaf28
Ola Embreus <embreus@student.chalmers.se>
2013-06-10 17:15:03 +0200

    animation script changes from last week

--- scripts/animation_he5.py
+++ scripts/animation_he5.py
@@ -3,18 +3,16 @@ from imports import *
 from nhqm.bases import mom_space as mom
 from nhqm.problems import He5
 from nhqm.QM_helpers import energies
-from nhqm.bases.contours import triangle_contour, naive_triangle_contour, gauss_contour
+from nhqm.bases.gen_contour import triangle_contour, naive_triangle_contour, gauss_contour
 from nhqm.plot_helpers import *
 from numpy.linalg import norm
 from collections import namedtuple
 import matplotlib.animation as anim
 
-
 fig = plt.figure(1)
 fig.clf()
 ax = fig.add_subplot(111)
 ax.axis([-0.02, 0.7, -0.11, 0.01])
-
 
 problem = He5 
 order = 10*3
@@ -25,12 +23,10 @@ Q = QNums(l=1, j=1.5, J=0, M=0,
           m=[-1.5, -0.5, 0.5, 1.5], 
           E=range(order))
 k_max = 2.5
-
 peak_x = 0.17
 
-
 FPS = 24
-film_length = 6
+film_length = 12
 frames = film_length * FPS
 peak_ys = sp.linspace(0, 0.1, frames)
 
@@ -41,14 +37,16 @@ def init():
 def animate(i):
     ax.cla()
     ax.axis([-0.02, 0.7, -0.11, 0.01])
+    ax.set_xlabel('Re k / [fm$^{-1}$]', fontsize=20)
+    ax.set_ylabel('Im k / [fm$^{-1}$]', fontsize=20)
     peak_y = peak_ys[i]
     contour = triangle_contour(peak_x, peak_y, k_max, order/3)
     points, weights = contour   
     H = mom.hamiltonian(contour, problem, Q)
     eigvals, eigvecs = energies(H)
     k = sp.sqrt(2*problem.mass*eigvals)
-    mesh, = ax.plot(sp.real(points), sp.imag(points), 'og')
-    ks, = ax.plot(sp.real(k), sp.imag(k), 'ob')
+    mesh, = ax.plot(sp.real(points), sp.imag(points), 'o',color='0.5')
+    ks, = ax.plot(sp.real(k), sp.imag(k), 'or')
     return ks, mesh, 
 
 ani= anim.FuncAnimation(fig, animate, frames, init_func=init, interval=2, blit=False)
--- scripts/animation_polepos.py
+++ scripts/animation_polepos.py
@@ -10,16 +10,16 @@ from collections import namedtuple
 import matplotlib.animation as anim
 
 
-fig = plt.figure(1)
+fig = plt.figure(2)
 fig.clf()
 ax = fig.add_subplot(111)
-ax.axis([-0.02, 0.7, -0.11, 0.01])
+ax.axis([-0.02, 0.7, -0.15, 0.01])
 
 
 problem = He5 
-order = 10*3
-V0max = 70
-V0min = 40
+order = 20*3
+V0max = 60
+V0min = 35
 problem.Vso = -7.04
 QNums = namedtuple('qnums', 'l j J M E m')
 Q = QNums(l=1, j=1.5, J=0, M=0, 
@@ -27,15 +27,15 @@ Q = QNums(l=1, j=1.5, J=0, M=0,
           E=range(order))
 k_max = 2.5
 
-peak_x = 0.17
-peak_y = 0.07
+peak_x = 0.25
+peak_y = 0.15
 
 contour = triangle_contour(peak_x, peak_y, k_max, order/3)
 points, weights = contour   
 
 
-FPS = 24
-film_length = 6
+FPS = 40
+film_length = 20
 frames = film_length * FPS
 V0s = sp.linspace(-V0max, -V0min, frames)
 
@@ -45,16 +45,23 @@ def init():
 
 def animate(i):
     ax.cla()
-    ax.axis([-0.02, 0.7, -0.11, 0.01])
+    ax.axis([-0.01, 1, -0.2, 0.25])
+    ax.set_xlabel('Re k / [fm$^{-1}$]', fontsize=20)
+    ax.set_ylabel('Im k / [fm$^{-1}$]', fontsize=20)
     problem.V0 = V0s[i]
     H = mom.hamiltonian(contour, problem, Q)
     eigvals, eigvecs = energies(H)
+    
     k = sp.sqrt(2*problem.mass*eigvals)
-    mesh, = ax.plot(sp.real(points), sp.imag(points), 'og')
-    ks, = ax.plot(sp.real(k), sp.imag(k), 'ob')
-    return ks, mesh, 
+    for a, b in enumerate(k):
+        if sp.real(b)<1e-3:
+            k[a]=sp.real(b)+abs(sp.imag(b))*1j
+    mesh, = ax.plot(sp.real(points), sp.imag(points), 'o',color='0.5')
+    ks, = ax.plot(sp.real(k), sp.imag(k), 'or')
+    text = ax.text(0.5, 0.17, 'V0 = %.0f MeV'%(problem.V0), fontsize=24)
+    return ks, mesh, text
 
 ani= anim.FuncAnimation(fig, animate, frames, init_func=init, interval=2, blit=False)
-ani.save('polsvep_anim.mp4', fps=FPS)
+ani.save('polsvep_anim_HD.mp4', fps=FPS)
 
 plt.show()
